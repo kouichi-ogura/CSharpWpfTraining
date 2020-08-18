efaf8591aca613ce18ef668acba3156e95813770
kouichi-ogura <kouichi_ogura@sobal.co.jp>
2020-08-18 16:24:52 +0900

    implement phase4-3

--- Phase4/Phase4/MainWindow.xaml.cs
+++ Phase4/Phase4/MainWindow.xaml.cs
@@ -23,6 +23,50 @@ namespace Phase4
         public MainWindow()
         {
             InitializeComponent();
+            LoadSetting();
+        }
+
+        ~MainWindow()
+        {
+            // ウィンドウが消滅しているので、ここでは取得できない
+            //SaveSetting();
+        }
+
+        private void ButtonOpenDialog_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LoadSetting()
+        {
+            // 不要（？）
+            //Properties.Settings.Default.Reload();
+
+            this.Left = Properties.Settings.Default.ScreenLeft;
+            this.Top = Properties.Settings.Default.ScreenTop;
+            this.Width = Properties.Settings.Default.ScreenWidth;
+            this.Height = Properties.Settings.Default.ScreenHeight;
+
+            TextFileName.Text = Properties.Settings.Default.TextFileName;
+        }
+
+        private void SaveSetting()
+        {
+            Properties.Settings.Default.ScreenLeft   = this.Left;
+            Properties.Settings.Default.ScreenTop    = this.Top;
+            Properties.Settings.Default.ScreenWidth  = this.Width;
+            Properties.Settings.Default.ScreenHeight = this.Height;
+
+            TextFileName.Text = Properties.Settings.Default.TextFileName;
+
+            Properties.Settings.Default.TextFileName = TextFileName.Text;
+
+            Properties.Settings.Default.Save();
+        }
+
+        // xamlのClosingで定義したメソッド。ウィンドウ閉じるときに呼ばれる
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveSetting();
         }
     }
 }
