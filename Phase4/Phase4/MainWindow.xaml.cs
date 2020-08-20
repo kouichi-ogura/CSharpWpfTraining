785d2875ab237db3202694d70242fe3aaddb1b7d
kouichi-ogura <kouichi_ogura@sobal.co.jp>
2020-08-20 09:48:41 +0900

    save point

--- Phase4/Phase4/MainWindow.xaml.cs
+++ Phase4/Phase4/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Phase4
 {
@@ -45,7 +32,6 @@ namespace Phase4
             this.Top = Properties.Settings.Default.ScreenTop;
             this.Width = Properties.Settings.Default.ScreenWidth;
             this.Height = Properties.Settings.Default.ScreenHeight;
-
             TextFileName.Text = Properties.Settings.Default.TextFileName;
         }
 
@@ -55,9 +41,6 @@ namespace Phase4
             Properties.Settings.Default.ScreenTop    = this.Top;
             Properties.Settings.Default.ScreenWidth  = this.Width;
             Properties.Settings.Default.ScreenHeight = this.Height;
-
-            TextFileName.Text = Properties.Settings.Default.TextFileName;
-
             Properties.Settings.Default.TextFileName = TextFileName.Text;
 
             Properties.Settings.Default.Save();
