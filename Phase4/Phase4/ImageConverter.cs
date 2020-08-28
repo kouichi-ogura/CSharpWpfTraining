c70ec5385948cc950e55bbd6387e0a264bf7ca53
kouichi-ogura <kouichi_ogura@sobal.co.jp>
2020-08-28 16:31:39 +0900

    decord image

--- Phase4/Phase4/ImageConverter.cs
+++ Phase4/Phase4/ImageConverter.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace Phase4
 {
     public class ImageConverter : IValueConverter
     {
+        private readonly int m_ThumailDecodePixelWidth = 50;
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return new BitmapImage(new Uri(value.ToString()));
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri(value.ToString());
+            img.DecodePixelWidth = m_ThumailDecodePixelWidth;
+            img.EndInit();
+            return img;
         }
 
         //-----------------------------------------------------------------------------------------------
--- Phase4/Phase4/Thumnail.cs
+++ Phase4/Phase4/Thumnail.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.IO;
 
 namespace Phase4
 {
     public sealed class Thumnail
     {
-        public string FilePath { get; set; }
+        public string FileFullpath { get; set; }
         public string FileName { get; set; }
 
-        public Thumnail(string filepath)
+        public Thumnail(string file_fullpath)
         {
-            this.FilePath = filepath;
-            this.FileName = Path.GetFileName(filepath);
+            this.FileFullpath = file_fullpath;
+            this.FileName = Path.GetFileName(file_fullpath);
         }
     }
 }
